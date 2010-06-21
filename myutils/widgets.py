f8e2d8551af14fc5c82acb46217d3aeb2e7beea6
Colin Powell <colin.powell@me.com>
2010-06-21 13:40:04 -0400

    Cleaning up.

--- myutils/widgets.py
+++ myutils/widgets.py
@@ -7,22 +7,7 @@ from django.utils import simplejson
 from django.utils.safestring import mark_safe
 
 from tagging.models import Tag
-from recaptcha.client import captcha
 
-class ReCaptcha(forms.widgets.Widget):
-    recaptcha_challenge_name = 'recaptcha_challenge_field'
-    recaptcha_response_name = 'recaptcha_response_field'
-
-    def render(self, name, value, attrs=None):
-        return mark_safe(u'%s' % captcha.displayhtml(settings.RECAPTCHA_PUBLIC_KEY))
-
-    def value_from_datadict(self, data, files, name):
-        return [data.get(self.recaptcha_challenge_name, None), 
-            data.get(self.recaptcha_response_name, None)]
-            
-
-
- 
 def MakeAutoCompleteTagInput(tagged_object):
     class AutoCompleteTagInput(forms.TextInput):
         class Media:
