f4e064e1c9d4b06e29b74a277b245450ea805dde
Colin Powell <colin@onecardinal.com>
2011-02-02 08:46:56 -0500

    Added a google lat long lookup utility.

---
+++ myutils/utils.py
@@ -0,0 +1,14 @@
+import urllib
+import settings
+
+def google_lat_long(location):
+    key = settings.GOOGLE_API_KEY
+    output = "csv"
+    location = urllib.quote_plus(location)
+    request = "http://maps.google.com/maps/geo?q=%s&output=%s&key=%s" % (location, output, key)
+    data = urllib.urlopen(request).read()
+    dlist = data.split(',')
+    if dlist[0] == '200':
+        return "%s, %s" % (dlist[2], dlist[3])
+    else:
+        return ''
